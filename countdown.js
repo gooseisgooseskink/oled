60c1638ae5a68eefe6f02db3679e203b398c21fd
gooseisgooseskink <126208887+gooseisgooseskink@users.noreply.github.com>
2023-11-30 20:08:05 -0700

    Create countdown.js

---
+++ countdown.js
@@ -0,0 +1,31 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 31, 2023 00:00:00").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    // Calculate days, hours, minutes, and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the countdown
+    document.getElementById("days").innerHTML = formatTime(days);
+    document.getElementById("hours").innerHTML = formatTime(hours);
+    document.getElementById("minutes").innerHTML = formatTime(minutes);
+    document.getElementById("seconds").innerHTML = formatTime(seconds);
+
+    // If the countdown is over, display a message
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+// Helper function to format time with leading zeros
+function formatTime(time) {
+    return time < 10 ? "0" + time : time;
+}
